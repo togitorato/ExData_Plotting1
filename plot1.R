20e35978c883a465f45b11031f0dd47d9b1f39a2
togitorato <toygar.akin3@gmail.com>
2018-04-15 15:20:40 +0300

    Script and plots are added.

---
+++ plot1.R
@@ -0,0 +1,38 @@
+#Get the data set address
+url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+filename = "project_data.zip"
+
+#Download data set if it does not exist already.
+if (!file.exists(filename)){
+        download.file(url, destfile = filename, mode = "wb")
+}
+
+#Unzip the file
+if (!file.exists("household_power_consumption")){
+        unzip(filename)
+}
+
+#Read the lines and determine which ones to extract from the data set.
+lines <- readLines("household_power_consumption.txt")
+good <- grep("^[12]/2/2007", substr(lines, 1,8))
+
+#Read and construct the table with respect to desired lines.
+table <- read.table(text = lines[good], header = TRUE, sep = ";", na.strings = "?", 
+                    col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage",
+                                  "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+                    stringsAsFactors = FALSE)
+
+#Check if correct lines are read or not.
+head(table)
+tail(table)
+#It seems correct.
+
+#Combine the data and time variables to obtain full time information. Then transform into date class.
+table$realDate = paste(table$Date, table$Time)
+table$realDate = strptime(table$realDate, "%d/%m/%Y %H:%M:%S")
+
+#Plot
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+hist(table$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
